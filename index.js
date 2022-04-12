010ccef91d18bee5868c1bc4290b1a61ab39ffc5
JoseToitio <josetoitio@gmail.com>
2022-04-11 21:49:06 -0300

    começando do 0

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const userRotes = require('./routes/UserRotes');
 
 const app = express();
 
-app.use(express.json());
-
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
-app.use('/user', userRotes);
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
