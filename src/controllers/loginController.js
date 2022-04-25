9a24489c0107c192922506575106686ebf152eb3
JoseToitio <josetoitio@gmail.com>
2022-04-25 20:03:03 -0300

    corrige erros

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,7 +2,7 @@
 const loginService = require('../services/loginServices');
 const tokeng = require('../token/tokenGenerator');
 
-const login = async (req, res) => {  
+const login = async (req, res, next) => {  
   try {
     const { email, password } = req.body;
     const exist = await loginService.login(email, password);
@@ -10,7 +10,7 @@ const login = async (req, res) => {
     const token = tokeng({ email, password });
     return res.status(200).json({ token });
   } catch (error) {
-    return res.status(500).json(error.message);
+    next(error);
   }
 };
 
